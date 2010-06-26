10a7e611653865513fe3bfca22a2f333150c603b
Carla Souza <crlnh.a@gmail.com>
2010-06-26 00:56:39 -0300

    Xen Provider

--- lib/puppet/provider/libvirt/libvirt.rb
+++ lib/puppet/provider/libvirt/libvirt.rb
@@ -1,57 +1,31 @@
 require 'libvirt'
-Puppet::Type.type(:virt).provide(:libvirt) do
-	desc "Libvirt provider"
-	
-	defaultfor :operatinsystem => [:debian, :ubuntu]
+Puppet::Type.type(:virt2).provide(:libvirt) do
 
-	commands :install => "virt-install" 
+        desc "   -v, --hvm: full virtualization, 
+                -p, --paravirt: paravirtualizatoin"
 
+        commands :install => "/usr/bin/virt-install"
 
+        def create
+                p "*** Create"
+                @memory=512 # get it from manifest
+                @path="path=/local/carla/gsoc/vm10.qcow2" #get it from manifest
 
-	def create
-		p "*** Executing provider.create"
-#		install "--name", :name, " --ram ", :memory, " --disk path=",:virt_path," --import  --force"
-	
-		# Testing
-		# err: //virttest/Virt[guestxen]/ensure: change from false to installed failed: Execution of '/usr/bin/virt-install --name "Jun24" --ram 1024 --disk path="/local/carla/gsoc/vm10.qcow2" --import  --force' returned 2: Usage: virt-install --name NAME --ram RAM STORAGE INSTALL [options]
-		# virt-install: error: no such option: --name "Jun24" --ram 1024 --disk path
- 		# But if I execute this line, it works
-		install "--name Jun24 --ram 512 --disk path=/local/carla/gsoc/vm10.qcow2 --import"
-	end
+                install "--name", @resource[:name],"--ram",@memory, "--disk" ,@path,"--import","--noautoconsole","--force"
+        end
 
-	def destroy
-		p "*** Executing provider.destroy " #Debuging
-		@@conn = Libvirt::open("qemu:///session")
-                @@dom = @@conn.lookup_domain_by_name("Jun24")
-                @@dom.undefine
-	end
+        def destroy
+                p "** Destroy"
+        end
 
-	def status
-		p "*** Executing provider.status: " #Debuging
-		@@conn = Libvirt::open("qemu:///session")
+        def exists?
+                p "** Exists?"
+                @@conn = Libvirt::open("qemu:///session")
                 all = @@conn.list_domains + @@conn.list_defined_domains
-		p all.include? @resource[:name]
-		all.include? @resource[:name]
-	end
-	
-	def virt_type
-	end
+                p @resource[:name]
+                p all.include? @resource[:name]
+                all.include? @resource[:name]
+        end
 
-	def on_poweroff
-		#<on_poweroff>
-	end
-
-	def on_reboot
-		#<on_reboot>
-	end
-
-	def on_crash
-		#<on_crash>
-	end
-
-	def autoboot
-		@@conn = Libvirt::open("qemu:///session") #testing
-                @@dom = @@conn.lookup_domain_by_name("Jun24") #testing
-                @@dom.autostart = value
-	end
 end
+~              
