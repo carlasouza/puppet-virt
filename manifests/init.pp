2a3bdce5ecb18652494f5133d7261db4ad98e9dc
Carla Souza <crlnh.a@gmail.com>
2010-07-16 14:48:53 -0300

    Manifest init.pp updated with ubuntu, debian and fedora required packages

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,31 @@
 class virt {
 	case $operatingsystem {
 		debian: { include virt::debian }
+		ubuntu: { include virt::ubuntu }
+		fedora: { include virt::fedora }
 	}
 }
 
 class virt::debian {
 	package {
-		[ "linux-image-xen-686", "xen-tools", "ubuntu-virt-server", "python-vm-builder",
-			"kvm", "qemu", "qemu-kvm"]:
+		[ "linux-image-xen-686", "xen-hypervisor", "xen-tools", "xen-utils", 
+		"kvm", "qemu", "libvirt-bin", "virtinst", "libvirt-bin" ]:
 		ensure => latest;
     	}
+}
+
+class virt::ubuntu {
+	package {
+		[ "ubuntu-virt-server", "python-vm-builder", "kvm", "qemu", "qemu-kvm",
+		"ubuntu-xen-server", "libvirt-ruby" ]
+		ensure => latest;
+	}
+}
+
+class virt::fedora {
+	package {
+		[ "kvm", "qemu", "libvirt", "python-virtinst", "kernel-xen", "xen",
+		"ruby-libvirt"]
+		ensure => latest;
+	}
 }
