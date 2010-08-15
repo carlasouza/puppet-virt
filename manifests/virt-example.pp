610d0b809be6b90a22545651692c9143a2f06ae5
Matthew Cluver <matthew.cluver@gmail.com>
2010-08-15 13:46:04 -0700

    showing how easy it is to make a different class for different virt_type

--- manifests/virt-example.pp
+++ manifests/virt-example.pp
@@ -1,10 +1,13 @@
-class virt-examples {
+class xen-fullvirt-guest {
 	virt { "Jun29":
 		memory => 512,
 		virt_path => "/home/carla/ourgrid/images/worker-carla/disk0.qcow2",
 		ensure => installed,
 		virt_type => "xen_fullyvirt"
 	}
+}
+
+class ovz-guest {
 	virt { "101":
 		memory => 1024,
                 xml_file => "/etc/libvirt/qemu/systems/101.xml",
