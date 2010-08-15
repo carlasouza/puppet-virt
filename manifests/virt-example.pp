b5dc380f461df1a61e828e789c90495374df398f
Matthew Cluver <matthew.cluver@gmail.com>
2010-08-15 07:08:45 -0700

    created an openvz example as well for clarification

--- manifests/virt-example.pp
+++ manifests/virt-example.pp
@@ -1,8 +1,15 @@
-class virt-exemple {
+class virt-examples {
 	virt { "Jun29":
 		memory => 512,
 		virt_path => "/home/carla/ourgrid/images/worker-carla/disk0.qcow2",
 		ensure => installed,
 		virt_type => "xen_fullyvirt"
+	}
+	virt { "101":
+		memory => 1024,
+                xml_file => "/etc/libvirt/qemu/systems/101.xml",
+		tmpl_cache => "debian-5.0-x86",
+		ensure => installed,
+		virt_type => "openvz"
 	}
 }
