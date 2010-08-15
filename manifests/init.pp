0f721d3e49c2a22bc60f11f6c187bb8c5a998918
Matthew Cluver <matthew.cluver@gmail.com>
2010-08-15 08:05:46 -0700

    created the initial package differentiation depending on hypervisor type using the facter $virtual attribute, which should be working now nicely.

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,25 +7,56 @@ class virt {
 }
 
 class virt::debian {
-	package {
-		[ "linux-image-xen-686", "xen-hypervisor", "xen-tools", "xen-utils", 
-		"kvm", "qemu", "libvirt-bin", "virtinst", "libvirt-bin" ]:
-		ensure => latest;
-    	}
+        case $virtual {
+                xen | kvm: {
+	             package {
+		        [ "linux-image-xen-686", "xen-hypervisor", "xen-tools", "xen-utils", 
+		        "kvm", "qemu", "libvirt-bin", "virtinst", "libvirt-bin" ]:
+		        ensure => latest;
+                     }
+    	        }
+                openvzhn: {
+	             package {
+		        [ "linux-image-openvz-686", "vzctl", "vzquota", "vzdump",
+                        "libvirt-bin", "python-virtinst" ]:
+		        ensure => latest;
+                     }
+    	        }
+        }
 }
 
 class virt::ubuntu {
-	package {
-		[ "ubuntu-virt-server", "python-vm-builder", "kvm", "qemu", "qemu-kvm",
-		"ubuntu-xen-server", "libvirt-ruby" ]
-		ensure => latest;
-	}
+        case $virtual {
+                xen | kvm: {
+	             package {
+		        [ "ubuntu-virt-server", "python-vm-builder", "kvm", "qemu", "qemu-kvm",
+		        "ubuntu-xen-server", "libvirt-ruby" ]:
+		        ensure => latest;
+	             }
+    	        }
+                openvzhn: {
+	             package {
+		        [ "linux-openvz", "vzctl", "vzquota", "libvirt-bin", "python-virtinst" ]:
+		        ensure => latest;
+                     }
+    	        }
+        }
 }
 
 class virt::fedora {
-	package {
-		[ "kvm", "qemu", "libvirt", "python-virtinst", "kernel-xen", "xen",
-		"ruby-libvirt"]
-		ensure => latest;
-	}
+        case $virtual {
+                xen | kvm: {
+	             package {
+		        [ "kvm", "qemu", "libvirt", "python-virtinst", "kernel-xen", "xen",
+		        "ruby-libvirt" ]:
+		        ensure => latest;
+	             }
+    	        }
+                openvzhn: {
+	             package {
+		        [ "ovzkernel", "vzctl", "vzquota", "libvirt", "python-virtinst", "ruby-libvirt" ]:
+		        ensure => latest;
+                     }
+    	        }
+        }
 }
