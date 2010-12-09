3fc38f80548679efa6a63816929a9221ba3ad420
mh <mh@immerda.ch>
2010-12-10 00:44:27 +0100

    naming for lenny

--- manifests/libvirt/debian.pp
+++ manifests/libvirt/debian.pp
@@ -2,4 +2,7 @@ class virt::libvirt::debian inherits virt::libvirt::base {
   Package['python-virtinst']{
     name => 'virtinst',
   }
+  Package['ruby-libvirt']{
+    name => 'libvirt-ruby',
+  }
 }
