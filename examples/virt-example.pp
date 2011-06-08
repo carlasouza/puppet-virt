d5ccec06ce9b20b4cde3f39bc2cd658eba10df66
Carla Souza <crlnh.a@gmail.com>
2011-06-08 20:23:10 -0300

    File renaming

---
+++ examples/virt-example.pp
@@ -0,0 +1,19 @@
+class xen-fullvirt-guest {
+	virt { "Jun29":
+		memory => 512,
+		virt_path => "/home/carla/ourgrid/images/worker-carla/disk0.qcow2",
+		ensure => installed,
+		virt_type => "xen_fullyvirt"
+	}
+}
+
+class ovz-guest {
+	virt { "101":
+		memory => 1024,
+                vcpu => 4,
+                xml_file => "/etc/libvirt/qemu/systems/101.xml",
+		tmpl_cache => "debian-5.0-x86",
+		ensure => installed,
+		virt_type => "openvz"
+	}
+}
