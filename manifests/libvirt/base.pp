6f5c6e12daf4229ad316d017c0ccaa873d03b8b4
mh <mh@immerda.ch>
2011-07-06 23:45:56 +0200

    add script to generate mac addresses for virtualizations

--- manifests/libvirt/base.pp
+++ manifests/libvirt/base.pp
@@ -10,9 +10,13 @@ class virt::libvirt::base {
     require => Package['libvirt'],
   } 
 
-  file{'/usr/local/sbin/guest_starter':
-    source => 'puppet:///modules/virt/libvirt/guest_starter',
-    require => Package['libvirt'],
-    owner => root, group => 0, mode => 0700;
+  file{
+    '/usr/local/sbin/guest_starter':
+      source => 'puppet:///modules/virt/libvirt/guest_starter',
+      require => Package['libvirt'],
+      owner => root, group => 0, mode => 0700;
+    '/usr/local/sbin/generate_random_macaddress':
+      source => 'puppet:///modules/virt/libvirt/random_mac_address.rb',
+      owner => root, group => 0, mode => 0755;
   }
 }
