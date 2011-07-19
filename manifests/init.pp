24bf457255e6630b4d3660b4081db00176740e53
Carla Souza <crlnh.a@gmail.com>
2011-07-18 23:40:17 -0300

    Init manifest updated

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,113 +1,136 @@
 class virt {
+	case $virtual {
+		/^kvm/: { include virt::kvm }
+		/^xen/: { include virt::xen }
+		/^openvzhn/: { include virt::openvz }
+	}
+}
+
+class virt::xen {
 	case $operatingsystem {
-		debian: { include virt::debian }
-		ubuntu: { include virt::ubuntu }
-		fedora: { include virt::fedora }
+		debian: { include virt::xen::debian }
+		ubuntu: { include virt::xen::ubuntu }
+		fedora: { include virt::xen::fedora }
 	}
 }
 
-class virt::debian {
-	case $virtual {
-		kvm: {
-			package {
-				["kvm",
-				"virt-manager",
-				"libvirt",
-				"libvirt-python",
-				"python-virtinst",
-				"qemu",
-				"qemu-img",
-				"qspice-libs"]: 
-					ensure => latest; 
-			}
-		}
-		xen: {
-			package {
-				["linux-image-xen-686",
-				"xen-hypervisor",
-				"xen-tools",
-				"xen-utils"]:
-					ensure => latest;
-			}
-		}
-		openvzhn: {
-			package {
-				["linux-image-openvz-686",
-				"vzctl",
-				"vzquota",
-				"vzdump",
-				"libvirt-bin",
-				"python-virtinst" ]:
-					ensure => latest;
-			}
-		}
+class virt::kvm {
+	case $operatingsystem {
+		debian: { include virt::kvm::debian }
+		ubuntu: { include virt::kvm::ubuntu }
+		fedora: { include virt::kvm::fedora }
 	}
 }
 
-class virt::ubuntu {
-	case $virtual {
-		kvm: {
-			package {
-				["ubuntu-virt-server",
-				"python-vm-builder",
-				"kvm",
-				"qemu",
-				"qemu-kvm",
-				"libvirt-ruby"]:
-					ensure => latest;
-			}
-		}
-		xen: {
-			package {
-				["python-vm-builder",
-				"ubuntu-xen-server",
-				"libvirt-ruby"]:
-					ensure => latest;
-			}
-		}
-		openvzhn: {
-			package {
-				["linux-openvz",
-				"vzctl",
-				"vzquota",
-				"libvirt-bin",
-				"python-virtinst"]:
-					ensure => latest;
-			}
-		}
+class virt::openvz {
+	case $operatingsystem {
+		debian: { include virt::openvz::debian }
+		ubuntu: { include virt::openvz::ubuntu }
+		fedora: { include virt::openvz::fedora }
 	}
 }
 
-class virt::fedora {
-	case $virtual {
-		kvm: {
-			package {
-				["kvm",
-				"qemu",
-				"libvirt",
-				"python-virtinst",
-				"ruby-libvirt"]:
-					ensure => latest;
-			}
-		}
-		xen: {
-			package {
-				["kernel-xen",
-				"xen",
-				"ruby-libvirt"]:
-					ensure => latest;
-			}
-		}
-		openvzhn: {
-			package {
-				["ovzkernel",
-				"vzctl",
-				"vzquota",
-				"libvirt",
-				"python-virtinst",
-				"ruby-libvirt"]:
-					ensure => latest;
-			}
-		}
+##
+# KVM
+##
+
+class virt::kvm::debian {
+	package {
+		["kvm",
+		"virt-manager",
+		"libvirt",
+		"libvirt-python",
+		"python-virtinst",
+		"qemu",
+		"qemu-img",
+		"qspice-libs"]: 
+			ensure => latest; 
+	}
+}
+
+class virt::kvm::ubuntu {
+	package {
+		["ubuntu-virt-server",
+		"python-vm-builder",
+		"kvm",
+		"qemu",
+		"qemu-kvm",
+		"libvirt-ruby"]:
+			ensure => latest;
+	}
+}
+
+class virt::kvm::fedora {
+	package {
+		["kvm",
+		"qemu",
+		"libvirt",
+		"python-virtinst",
+		"ruby-libvirt"]:
+			ensure => latest;
+	}
+}
+
+##
+# Xen
+##
+
+class virt::xen::debian {
+	package {
+		["linux-image-xen-686",
+		"xen-hypervisor",
+		"xen-tools",
+		"xen-utils"]:
+			ensure => latest;
+	}
+}
+class virt::xen::ubuntu {
+	package {
+		["python-vm-builder",
+		"ubuntu-xen-server",
+		"libvirt-ruby"]:
+			ensure => latest;
+	}
+}
+class virt::xen::fedora {
+	package {
+		["kernel-xen",
+		"xen",
+		"ruby-libvirt"]:
+			ensure => latest;
+	}
+}
+
+##
+# OpenVZ
+##
+
+class virt::openvzhn::debian {
+	package {
+		["linux-image-openvz-686",
+		"vzctl",
+		"vzquota",
+		"vzdump"]:
+			ensure => latest;
+	}
+}
+
+
+class virt::openvzhn::ubuntu {
+	package {
+		["linux-openvz",
+		"vzctl",
+		"vzquota"]:
+			ensure => latest;
+	}
+}
+
+
+class virt::openvzhn::fedora {
+	package {
+		["ovzkernel",
+		"vzctl",
+		"vzquota"]:
+			ensure => latest;
 	}
 }
