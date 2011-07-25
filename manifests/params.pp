0032e0ceafddf7783e61d8d909aceb68bec86667
windowsrefund <windowsrefund@gmail.com>
2011-07-25 11:54:10 -0400

    ve definition now supports overrides for memory and ve_dir

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,44 +1,51 @@
 class virt::params {
-	
-	case $virtual {
-		/^openvzhn/: {
-			$service = 'vz'
-			$basedir = '/etc/vz/'
-			$confdir = '/etc/vz/conf/'
-			$vedir = '/var/lib/vz/' #Update here with your custom value
-		} 
-	}
-	case $operatingsystem {
-	
-		debian: {
-		
-			$packages = $virtual ? {
-				kvm => [ 'kvm', 'virt-manager', 'libvirt', 'libvirt-python', 'python-virtinst', 'qemu', 'qemu-img', 'qspice-libs' ], 
-				xen => [ 'linux-image-xen-686', 'xen-hypervisor', 'xen-tools', 'xen-utils' ],
-				openvzhn =>  [ "linux-image-${kernelmajversion}-openvz-686", 'vzctl', 'vzquota' ],
-			}
-		}
-	
-		ubuntu: {
-			$packages = $virtual ? {
-				kvm => [ 'ubuntu-virt-server', 'python-vm-builder', 'kvm', 'qemu', 'qemu-kvm', 'libvirt-ruby' ],
-				xen => [ 'python-vm-builder', 'ubuntu-xen-server', 'libvirt-ruby' ],
-				openvzhn =>  [ "linux-image-${kernelmajversion}-openvz-686", 'vzctl', 'vzquota' ],
-			}
-		}
-	
-		fedora: {
-			# FIXME: not tested
-			$packages = $virtual ? {
-				kvm => [ 'kvm', 'qemu', 'libvirt', 'python-virtinst', 'ruby-libvirt' ],
-				xen => [ 'kernel-xen', 'xen', 'ruby-libvirt' ],
-				openvzhn =>  [ 'ovzkernel', 'vzctl', 'vzquota' ],
-			}
-		
-		}
-	
-		default: {
-			fail("This module is not supported on $operatingsystem")
-		}
-	}
+
+    case $operatingsystem {
+
+        # Each variable should really be broken out depending on $virtual but I'm not doing that now since
+        # I am only working with openvz
+        debian: {
+            $service = 'vz'
+            $basedir = '/etc/vz'
+            $confdir = '/etc/vz/conf'
+            $vedir = '/vz'
+            $packages = $virtual ? {
+              kvm => [ 'kvm', 'virt-manager', 'libvirt', 'libvirt-python', 'python-virtinst', 'qemu', 'qemu-img', 'qspice-libs' ], 
+              xen => [ 'linux-image-xen-686', 'xen-hypervisor', 'xen-tools', 'xen-utils' ],
+              openvzhn =>  [ "linux-image-${kernelmajversion}-openvz-686", 'vzctl', 'vzquota' ],
+            }
+        }
+
+        ubuntu: {
+            $service = 'vz'
+            $basedir = '/etc/vz'
+            $confdir = '/etc/vz/conf'
+            $vedir = '/vz'
+            $packages = $virtual ? {
+              kvm => [ 'ubuntu-virt-server', 'python-vm-builder', 'kvm', 'qemu', 'qemu-kvm', 'libvirt-ruby' ],
+              xen => [ 'python-vm-builder', 'ubuntu-xen-server', 'libvirt-ruby' ],
+              openvzhn =>  [ "linux-image-${kernelmajversion}-openvz-686", 'vzctl', 'vzquota' ],
+            }
+        }
+
+        fedora: {
+            # I don't run this distribution so fix if needed - AK
+            $service = 'vz'
+            $basedir = '/etc/vz'
+            $confdir = '/etc/vz/conf'
+            $vedir = '/vz'
+            $packages = $virtual ? {
+              kvm => [ 'kvm', 'qemu', 'libvirt', 'python-virtinst', 'ruby-libvirt' ],
+              xen => [ 'kernel-xen', 'xen', 'ruby-libvirt' ],
+              openvzhn =>  [ 'ovzkernel', 'vzctl', 'vzquota', 'libvirt', 'python-virtinst', 'ruby-libvirt' ],
+            }
+
+        }
+
+        default: {
+            fail("This module is not supported on $operatingsystem")
+        }
+
+    }
+        
 }
--- manifests/ve.pp
+++ manifests/ve.pp
@@ -1,8 +1,7 @@
-# A wrapper for virt module used to manage a OpenVZ VE
-# Author: Adam Kosmin
+# A wrapper for Carla's virt module used to manage a OpenVZ VE
 #
 # Required modules:
-# - lvm: http://forge.puppetlabs.com/puppetlabs/lvm
+# - lvm (patched): https://github.com/windowsrefund/puppet-lvm
 # - iptables: http://bob.sh/puppet-iptables
 #
 # Performs the following when ensure => present:
@@ -45,13 +44,13 @@
 # See http://bob.sh/puppet-iptables for more info on the iptables type
 #
 # iptables { 
-#	nat-ves:
-#		proto => 'all',
-#		table => 'nat',
-#		chain => 'POSTROUTING',
-#		source => "${network_eth0}/${netmask_eth0}",
-#		outiface => 'eth0',
-#		jump => 'MASQUERADE';
+# nat-ves:
+#   proto => 'all',
+#   table => 'nat',
+#   chain => 'POSTROUTING',
+#   source => "${network_eth0}/${netmask_eth0}",
+#   outiface => 'eth0',
+#   jump => 'MASQUERADE';
 #    forward-ves-out:
 #        proto => 'all',
 #        chain => 'FORWARD',
@@ -61,11 +60,11 @@
 #    forward-ves-in:
 #        proto => 'all',
 #        chain => 'FORWARD',
-#        state => [ 'RELATED', 'ESTABLISHED' ],	
+#        state => [ 'RELATED', 'ESTABLISHED' ], 
 #        iniface => 'eth0',
 #        outiface => 'venet0',
 #        jump => 'ACCEPT';
-#	} 
+# } 
 #
 # virt::ve { helloworld:
 #    os_template => "debian-6.0-${architecture}-reliant",
@@ -74,107 +73,117 @@
 #    require => Iptables[nat-ves, forward-ves-out, forward-ves-in],
 # }
 #
-
 define virt::ve (
-	$ensure = 'present',
-	$puppetize = true,
-	$os_template = undef,
-	$ipaddr = undef,
-	$nameserver = undef,
-	$configfile = 'basic',
-	$virt_type = 'openvz',
-	$lvm_vg = 'vg0',
-	$lvm_pv = '/dev/sda3',
-	$lvm_fstype = 'ext4',
-	$lvm_size = '1G'
+  $ensure = 'present',
+  $puppetize = true,
+  $os_template = undef,
+  $tmpl_repo = undef,
+  $ipaddr = undef,
+  $nameserver = undef,
+  $configfile = 'basic',
+  $virt_type = 'openvz',
+  $lvm_vg = 'vg0',
+  $lvm_pv = '/dev/sda3',
+  $lvm_fstype = 'ext4',
+  $lvm_size = '1G',
+  $memory = 256,
+  $vedir_override = ''
 ) {
 
-    if $os_template == undef {
-        fail("The os_template parameter must be defined.")
-    }
+  if $os_template == undef {
+      fail("The os_template parameter must be defined.")
+  }
 
-    include virt 
-    include virt::params
+  include virt 
+  include virt::params
 
-    virt { $name:
-        ensure => $ensure ? {
-            absent => 'stopped',
-            default => 'running',
-        },
-        virt_type => $virt_type,
-        os_template => $os_template,
-        configfile => $configfile,
-        ipaddr => $ipaddr,
-        ve_root => "${virt::params::vedir}/${name}/root",
-        ve_private => "${virt::params::vedir}/${name}/private",
-        nameserver => $nameserver,
-        require => $ensure ? {
-            present => Mount["${virt::params::vedir}/$name"],
-            default => undef,
-        },
-    }
+  if ($vedir_override != '') {
+    $vedir = $vedir_override
+  } else {
+    $vedir = $virt::params::vedir
+  }
 
-	# mount when the VE is present. Otherwise, unmount it
-	mount { "${virt::params::vedir}/$name":
-		ensure => $ensure ? {
-			absent => unmounted,
-			default => mounted,
-		},
-		fstype => $lvm_fstype,
-		options => 'rw,noatime',
-		device => "/dev/mapper/${lvm_vg}-$name",
-		require => $ensure ? {
-            absent => Virt[$name],
-            default => [ Lvm::Volume[$name], File ["${virt::params::vedir}/$name"] ],
-        },
-	}
-		
-	file { "${virt::params::vedir}/$name":
-        ensure => directory,
-        owner => 'root',
-        group => 'root',
-        mode => 0755,
-    }
+  virt { $name:
+    ensure => $ensure ? {
+        absent => 'stopped',
+        default => 'running',
+    },
+    virt_type => $virt_type,
+    os_template => $os_template,
+    tmpl_repo => $tmpl_repo,
+    configfile => $configfile,
+    ipaddr => $ipaddr,
+    ve_root => "${vedir}/${name}/root",
+    ve_private => "${vedir}/${name}/private",
+    nameserver => $nameserver,
+    memory => $memory,
+    require => $ensure ? {
+      present => Mount["${vedir}/$name"],
+      default => undef,
+    },
+  }
+
+  # mount when the VE is present. Otherwise, unmount it
+  mount { "${vedir}/$name":
+    ensure => $ensure ? {
+      absent => unmounted,
+      default => mounted,
+    },
+    fstype => $lvm_fstype,
+    options => 'rw,noatime',
+    device => "/dev/mapper/${lvm_vg}-$name",
+    require => $ensure ? {
+      absent => Virt[$name],
+      default => [ Lvm::Volume[$name], File ["${vedir}/$name"] ],
+    },
+  }
+    
+  file { "${vedir}/$name":
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => 0755,
+  }
  
-	lvm::volume { $name:
-		ensure => present,
-		vg => $lvm_vg,
-		pv => $lvm_pv,
-		fstype => $lvm_fstype,
-		size => $lvm_size,
-	}
+  lvm::volume { $name:
+    ensure => present,
+    vg => $lvm_vg,
+    pv => $lvm_pv,
+    fstype => $lvm_fstype,
+    size => $lvm_size,
+  }
 
     if ($puppetize) and ($ensure == 'present') {
 
-        # A message from Adam on this ghetto approach to managing
-        # /etc/hosts in the VE: Using an exec to call vzctl exec
-        # <ctid> puppet apply -e 'host ... either doesn't work or I'm
-        # just not smart enough to escape it properly.  Augeas appears
-        # to be unable to "see" files which belong to a VE.
+      # A message from Adam on this ghetto approach to managing
+      # /etc/hosts in the VE: Using an exec to call vzctl exec
+      # <ctid> puppet apply -e 'host ... either doesn't work or I'm
+      # just not smart enough to escape it properly.  Augeas appears
+      # to be unable to "see" files which belong to a VE.
 
-        # augtool print /files/etc/sudoers | wc -l 
-        # 20 
-        # augtool print /files/vz/101/private/etc/sudoers | wc -l 
-        # 0
-        #
-        file { "ve-hosts-${name}":
-            ensure => present,
-            path => "${virt::params::vedir}/${name}/private/etc/hosts",
-            owner => 'root',
-            group => 'root',
-            mode => 0644,
-            content => "127.0.0.1\t${name} localhost\n${serverip}\tpuppet\n",
-            require => Virt[$name],
-            noop => $ensure ? { absent => true, default => false, };
-        }
+      # augtool print /files/etc/sudoers | wc -l 
+      # 20 
+      # augtool print /files/vz/101/private/etc/sudoers | wc -l 
+      # 0
+      #
+      file { "ve-hosts-${name}":
+        ensure => present,
+        path => "${vedir}/${name}/private/etc/hosts",
+        owner => 'root',
+        group => 'root',
+        mode => 0644,
+        content => "127.0.0.1\t${name} localhost\n${serverip}\tpuppet\n",
+        require => Virt[$name],
+        noop => $ensure ? { absent => true, default => false, };
+      }
 
-        exec { "puppetize-${name}":
-            command => "vzctl exec2 $name 'puppet agent -t -l /tmp/install.log --pluginsync true'",
-            unless => "vzctl exec2 $name 'crontab -l | grep -q puppet-client'",
-            timeout => 300,
-            returns => [ 0, 2 ],
-            require => File["ve-hosts-${name}"],
-        }
+      exec { "puppetize-${name}":
+        command => "vzctl exec2 $name 'puppet agent -t -l /tmp/install.log --pluginsync true'",
+        unless => "vzctl exec2 $name 'crontab -l | grep -q puppet-client'",
+        timeout => 300,
+        returns => [ 0, 2 ],
+        require => File["ve-hosts-${name}"],
+      }
 
     }
 
