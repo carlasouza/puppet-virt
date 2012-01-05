ea372bc98b60ecf17657ebbdf23af3e78c8f0e98
Steve Huff <shuff@vecna.org>
2012-01-05 11:11:36 -0500

    try to encapsulate the connect method

--- lib/facter/virt_libvirt.rb
+++ lib/facter/virt_libvirt.rb
@@ -1,9 +1,21 @@
 # virt_libvirt.rb
-require 'libvirt'
+
+def libvirt_connect
+  begin
+    require 'libvirt'
+    Libvirt::open('qemu:///system')
+  rescue Libvirt::Error => e
+    if e.libvirt_code == 3
+      # do nothing
+    else
+      raise
+    end
+  end
+end
 
 Facter.add("virt_conn_type") do
+  confine :kernel => "Linux"
   setcode do
-    conn = Libvirt::open('qemu:///system')
-    conn.type.chomp
+    libvirt_connect.type.chomp
   end
 end
