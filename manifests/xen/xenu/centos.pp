a476c4b321e0334ef7bb2b946b653e3bd117b081
mh <mh@immerda.ch>
2012-06-17 16:45:50 -0300

    line is deprecated better use file_line

--- manifests/xen/xenu/centos.pp
+++ manifests/xen/xenu/centos.pp
@@ -1,11 +1,11 @@
 class virt::xen::xenu::centos {
   # Connect console to xvc0
-  line{
+  file_line{
     'xvc0_inittab':
-      file => '/etc/inittab',
+      path => '/etc/inittab',
       line => 'co:2345:respawn:/sbin/agetty xvc0 9600 vt100-nav';
     'securetty':
-      file => '/etc/securetty',
+      path => '/etc/securetty',
       line => 'xvc0';
   }
 }
