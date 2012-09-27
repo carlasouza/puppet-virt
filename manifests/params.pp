ee351fb82d39c888bfdfb4e5bf288bb7ecee3d91
Carla Souza <contact@carlasouza.com>
2012-09-27 18:45:19 -0300

    Refactoring

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,43 +5,32 @@ class virt::params {
       $servicename = 'vz'
       $basedir = '/etc/vz/'
       $confdir = '/etc/vz/conf/'
-      $vedir = '/var/lib/vz/' #XXX: Update here with your custom value
+      $vedir = '/var/lib/vz/' # You can update here with your custom value
+      $packages = $operatingsystem ? {
+        Debian => [ "linux-image-${kernelmajversion}-openvz-686", 'vzctl', 'vzquota' ],
+        Ubuntu => [ "linux-image-${kernelmajversion}-openvz-686", 'vzctl', 'vzquota' ],
+        Fedora => [ 'ovzkernel', 'vzctl', 'vzquota' ],
+      }
+    }
+    /^physical|^kvm/: {
+      $servicename = 'libvirtd'
+      $packages = $operatingsystem ? {
+        Debian => [ 'kvm', 'virt-manager', 'libvirt', 'python-virtinst', 'qemu', 'qemu-img' ],
+        Ubuntu => [ 'ubuntu-virt-server', 'python-vm-builder', 'kvm', 'qemu', 'qemu-kvm', 'libvirt-ruby' ],
+        Fedora => [ 'kvm', 'qemu', 'libvirt', 'python-virtinst', 'ruby-libvirt' ],
+      }
+    }
+    /^xen/: {
+      $servicename = 'libvirtd'
+      $packages = $operatingsystem ? {
+        Debian => [ 'linux-image-xen-686', 'xen-hypervisor', 'xen-tools', 'xen-utils' ],
+        Ubuntu => [ 'python-vm-builder', 'ubuntu-xen-server', 'libvirt-ruby' ],
+        Fedora => [ 'kernel-xen', 'xen', 'ruby-libvirt' ],
+      }
     }
     default: {
       $servicename = 'libvirtd'
     }
   }
 
-  case $::operatingsystem {
-    Debian: {
-
-      $packages = $virtual ? {
-        physical, kvm => [ 'kvm', 'virt-manager', 'libvirt', 'python-virtinst', 'qemu', 'qemu-img' ],
-        xen => [ 'linux-image-xen-686', 'xen-hypervisor', 'xen-tools', 'xen-utils' ],
-        openvzhn =>  [ "linux-image-${kernelmajversion}-openvz-686", 'vzctl', 'vzquota' ],
-      }
-    }
-
-    Ubuntu: {
-      $packages = $virtual ? {
-        physical, kvm => [ 'ubuntu-virt-server', 'python-vm-builder', 'kvm', 'qemu', 'qemu-kvm', 'libvirt-ruby' ],
-        xen => [ 'python-vm-builder', 'ubuntu-xen-server', 'libvirt-ruby' ],
-        openvzhn =>  [ "linux-image-${kernelmajversion}-openvz-686", 'vzctl', 'vzquota' ],
-      }
-    }
-
-    Fedora: {
-      # FIXME: not tested
-      $packages = $virtual ? {
-        physical, kvm => [ 'kvm', 'qemu', 'libvirt', 'python-virtinst', 'ruby-libvirt' ],
-        xen => [ 'kernel-xen', 'xen', 'ruby-libvirt' ],
-        openvzhn =>  [ 'ovzkernel', 'vzctl', 'vzquota' ],
-      }
-
-    }
-
-    default: {
-      fail ("This module is not supported on $operatingsystem")
-    }
-  }
 }
